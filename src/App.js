8d6f272bb0d4d8e5122c6428498d252390522a08
Jay Brewer <97116059+jaybrewer49@users.noreply.github.com>
2022-06-17 13:54:40 -0400

    adding componets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import {useEffect, useState } from 'react'
+import Gallery from './componets/Gallery'
+import SearchBar from './componets/SearchBar'
 
-
-function App() {
+function App(){
+  let [search, setSearch] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
+  
   return (
-    <div className="App">
-
+    <div>
+        <SearchBar />
+        {message}
+        <Gallery />
     </div>
-  );
+  )
 }
 
 export default App;
